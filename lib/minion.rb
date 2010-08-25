8e30c4b7cd2f25e9809f14d3c1de0bb55b8e1ce9
Tushar <tusharranka@gmail.com>
2010-08-24 17:16:53 -0700

    merge in changes from minion monkey patch - any error forces a reset of bunny, retry to connect to the server if it goes down, use durable => false queues, use :ack => false queues subscriptions, use yaml encoding instead of json so that we can handle ruby symbols

--- lib/minion.rb
+++ lib/minion.rb
@@ -5,28 +5,40 @@ require 'bunny'
 require 'minion/handler'
 
 module Minion
+  
+  class AMQPServerConnectError < StandardError; end;
+  
+  NUMBER_OF_RETRIES = 3
+  
 	extend self
 
 	def url=(url)
 		@@config_url = url
 	end
 
-	def enqueue(jobs, data = {})
-		raise "cannot enqueue a nil job" if jobs.nil?
-		raise "cannot enqueue an empty job" if jobs.empty?
+  def enqueue(jobs, data = {})
+    raise "cannot enqueue a nil job" if jobs.nil?
+    raise "cannot enqueue an empty job" if jobs.empty?
 
-		## jobs can be one or more jobs
-		if jobs.respond_to? :shift
-			queue = jobs.shift
-			data["next_job"] = jobs unless jobs.empty?
-		else
-			queue = jobs
-		end
+    ## jobs can be one or more jobs
+    if jobs.respond_to? :shift
+      queue = jobs.shift
+      data["next_job"] = jobs unless jobs.empty?
+    else
+      queue = jobs
+    end
 
-		encoded = JSON.dump(data)
-		log "send: #{queue}:#{encoded}"
-		bunny.queue(queue, :durable => true, :auto_delete => false).publish(encoded)
-	end
+    # NOTE: we encode to yaml, Minion encodes to json.  The reason is that json cannot handle Ruby symbols    
+    yamlized_data = data.to_yaml
+    log "send: #{queue}:#{yamlized_data}"
+
+    # NOTE: When rabbitMQ goes down, the mongrel keeps on holding the stale connection to it. We need to establish a new
+    # connection when this happens.
+    
+    with_bunny_connection_retry do
+      bunny.queue(queue, :durable => false, :auto_delete => false).publish(yamlized_data)
+    end
+  end
 
 	def log(msg)
 		@@logger ||= proc { |m| puts "#{Time.now} :minion: #{m}" }
@@ -46,11 +58,12 @@ module Minion
 		handler.when = options[:when] if options[:when]
 		handler.unsub = lambda do
 			log "unsubscribing to #{queue}"
-			MQ.queue(queue, :durable => true, :auto_delete => false).unsubscribe
+			MQ.queue(queue, :durable => false, :auto_delete => false).unsubscribe
 		end
+
 		handler.sub = lambda do
 			log "subscribing to #{queue}"
-			MQ.queue(queue, :durable => true, :auto_delete => false).subscribe(:ack => true) do |h,m|
+			MQ.queue(queue, :durable => false, :auto_delete => false).subscribe(:ack => false) do |h,m|
 				return if AMQP.closing?
 				begin
 					log "recv: #{queue}:#{m}"
@@ -64,7 +77,6 @@ module Minion
 					raise unless error_handler
 					error_handler.call(e,queue,m,h)
 				end
-				h.ack
 				check_all
 			end
 		end
@@ -85,27 +97,53 @@ module Minion
 		@@handlers.each { |h| h.check }
 	end
 
-	def run
-		log "Starting minion"
+  def run
+    log "Starting minion"
 
-		Signal.trap('INT') { AMQP.stop{ EM.stop } }
-		Signal.trap('TERM'){ AMQP.stop{ EM.stop } }
+    Signal.trap('INT') { exit_handler }
+    Signal.trap('TERM'){ exit_handler }
 
-		EM.run do
-			AMQP.start(amqp_config) do
-				MQ.prefetch(1)
-				check_all
-			end
-		end
-	end
+    EM.run do
+      connection = AMQP.start(amqp_config) do
+        MQ.prefetch(1)
+        check_all
+      end
+      
+      # NOTE: By default, Minion gets into a wierd state when there is a connection error (i.e., the AMQP server is down).  
+      # In short, it continues running without any subscriptions. We chose to detect this case, and kill the worker that
+      # has invoke run. This will allow it to be restarted by the monitoring system, and hopefully by then, the AMQP
+      # server will be back up. 
+      if connection.error?
+        exit_handler
+        raise AMQPServerConnectError.new("Couldn't connect to RabbitMQ server at #{amqp_url}")
+      end
+    end
+  end
 
 	def amqp_url
 		@@amqp_url ||= ENV["AMQP_URL"] || "amqp://guest:guest@localhost/"
 	end
 
-	def amqp_url=(url)
-		@@amqp_url = url
-	end
+  def amqp_url=(url)
+    clear_bunny
+    @@amqp_url = url
+  end
+  
+  def delete!(*queue_names)
+    EM.run do 
+      AMQP.start(amqp_config) do
+        queue_names.each do |queue_name|
+          MQ.queue(queue_name).delete
+          msg = "Minion: deleting '#{queue_name}' queue from the AMQP server."
+          ActiveRecord::Base::logger.info(msg)
+          log msg
+        end
+      end
+
+      AMQP.stop { EM.stop }
+    end
+
+  end
 
 	private
 
@@ -140,5 +178,37 @@ module Minion
 	def error_handler
 		@@error_handler ||= nil
 	end
+	
+	def exit_handler
+    AMQP.stop { EM.stop } 
+  end
+
+  def with_bunny_connection_retry
+    num_executions = 1 
+    begin
+      yield
+    rescue Bunny::ServerDownError, Bunny::ConnectionError => e
+      clear_bunny
+      if num_executions < NUMBER_OF_RETRIES
+        log "Retry ##{num_executions}  : #{caller.first}"
+        num_executions += 1
+        sleep 0.5
+        retry
+      else
+        error = e.class.new(e.message + " (Retried #{NUMBER_OF_RETRIES} times.  Giving up.)")
+        error.set_backtrace(e.backtrace)
+        raise error
+      end
+    rescue => e
+      clear_bunny
+      raise e
+    end            
+  end
+
+  def clear_bunny
+    @@bunny = nil    
+  end
+  
+  
 end
 
